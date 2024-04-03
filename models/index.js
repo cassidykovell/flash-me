6171e35005da256da1e89181a3e1fb6d3a00bfeb
unknown <rnaz.alluri@gmail.com>
2024-04-02 21:25:13 -0700

    update commit?

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Flashcard = require('./Flashcard'); // Rename Project to Flashcard
-const Collection = require('/Collection');
+const Collection = require('./Collection');
+const Flashcard = require('./flashcard');
 
 User.hasMany(Collection, {
   foreignKey: 'user_id',
@@ -9,14 +9,14 @@ User.hasMany(Collection, {
 
 Collection.belongsTo(User, {
   foreignKey: 'user_id',
-})
+});
 
 Collection.hasMany(Flashcard, {
-  foreignKey: "collection_id"
-})
-
-Flashcard.belongsTo(Collection, {
   foreignKey: 'collection_id'
 });
 
-module.exports = { User, Flashcard }; // Update Project to Flashcard
+Flashcard.belongsTo(Collection, {
+  foreignKey: 'collection_id',
+});
+
+module.exports = { User, Collection, Flashcard };
