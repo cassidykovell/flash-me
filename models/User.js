941c0e89552bc696c6bd8a99b78c77aae2078bf9
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-04-11 20:56:33 -0700

    fixing issue

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt'); 
 const sequelize = require('../config/connection'); 
+//fix commit 
 
 class User extends Model {
   checkPassword(loginPw) {
