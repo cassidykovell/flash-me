3fb122defffbae6e60b6c32dc018f77dccccfbdb
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-04-11 21:53:36 -0700

    fixing issue

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    logging: console.log, // Enable logging to console
-  });
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -14,19 +13,9 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-      logging: console.log,
+      port: 3306
     }
   );
 }
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
 
 module.exports = sequelize;
